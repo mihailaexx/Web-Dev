ffc437c6232bb42651ec08efea3e0c4a3a9894d6
mihailaexx <m_bulushev@kbtu.kz>
2025-03-23 13:45:08 +0500

    .

---
+++ lab8/api/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+
+# Register your models here.
+
+from .models import Category, Product
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "name")
+    search_fields = ("id", "name",)
+    ordering = ("id",)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "price", "description", "count", "is_active", "category")
+    search_fields = ("id", "name",)
+    ordering = ("id", "is_active", "category")
+    list_filter = ("is_active", "category")
+
+    prepopulated_fields = {"description": ("name",)}
---
+++ lab8/api/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.FloatField()
+    description = models.TextField()
+    count = models.IntegerField(default=1)
+    is_active = models.BooleanField(default=True)
+    category = models.ForeignKey(to=Category, on_delete=models.CASCADE, default=1)
+
+    class Meta:
+        verbose_name = "Product"
+        verbose_name_plural = "Products"
+
+    def __str__(self):
+        return f"{self.name} ({self.count})"
